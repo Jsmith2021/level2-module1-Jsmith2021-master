4289afd6bb96737567f97329dd5d08ada14ad643
U have'nt <whaaqaaaidchbfgvruihgiudfgiufhjifo@gmail.com>
2018-12-23 12:53:06 -0800

    12/23/18

--- src/RainGamePackage/RainGamePanel.java
+++ src/RainGamePackage/RainGamePanel.java
@@ -15,12 +15,15 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 import intro_to_array_lists.LeagueInvaders;
+import intro_to_array_lists.ObjectManager;
+import intro_to_array_lists.Projectile;
+import intro_to_array_lists.RocketShip;
 
 public class RainGamePanel extends JPanel implements ActionListener, KeyListener {
 	final int MENU_STATE = 0;
 	final int GAME_STATE=1;
-	final int INSTRUCTIONS_STATE = 2;
-	final int END_STATE = 3;
+	final int INSTRUCTIONS_STATE = -1;
+	final int END_STATE = 2;
 	int currentState= MENU_STATE;
 	Font menuFont;
 	Font startFont;
@@ -32,22 +35,28 @@ public class RainGamePanel extends JPanel implements ActionListener, KeyListener
 	Timer rainTimer;
 	Font instructions;
 	Font instructions1;
+	Font instructionsTitle;
 	  public static BufferedImage rain;
 	  public static BufferedImage rainpics;
 	  public static BufferedImage sunny;
+	  public static BufferedImage rainyday;
 	
 RainGamePanel(){
 	rainTimer=new Timer(1000/60,this);
 	menuFont=new Font("Arial", Font.BOLD, 48);
 	startFont=new Font("Arial", Font.PLAIN, 24);
 	instructionsFont=new Font("Arial", Font.PLAIN, 24);
-	endFont=new Font("Arial", Font.BOLD, 48);
+	instructions=new Font("Arial", Font.PLAIN, 24);
+	instructions1=new Font("Arial", Font.PLAIN, 24);
+	endFont=new Font("Arial", Font.PLAIN, 48);
 	endFont2=new Font("Arial", Font.PLAIN, 24);
 	restart=new Font("Arial", Font.PLAIN, 24);
+	instructionsTitle=new Font("Arial", Font.BOLD, 36);
 	 try {
          rain = ImageIO.read(this.getClass().getResourceAsStream("rainpic.jpg"));
          rainpics = ImageIO.read(this.getClass().getResourceAsStream("rain.jpg"));
          sunny = ImageIO.read(this.getClass().getResourceAsStream("sunnyday.jpg"));
+        // rainyGamePage = ImageIO.read(this.getClass().getResourceAsStream("rainyday.jpg"));
 
  } catch (IOException e) {
 
@@ -74,7 +83,7 @@ void updateEndState() {
 }
 
 void drawGameState(Graphics g) {
-	g.drawImage(rain, 0,0, RainCatcher.WIDTH, RainCatcher.HEIGHT, null);
+	g.drawImage(rainpics, 0,0, RainCatcher.WIDTH, RainCatcher.HEIGHT, null);
 	
 	
 }
@@ -82,36 +91,38 @@ void drawEndState(Graphics g) {
 	g.drawImage(sunny, 0,0, RainCatcher.WIDTH, RainCatcher.HEIGHT, null);
 	g.setFont(endFont);
 	g.setColor(Color.WHITE);
-	g.drawString("Game Over!", 30, 100);
+	g.drawString("Game Over!", 120, 150);
 	g.setFont(endFont2);
 	g.setColor(Color.WHITE);
-	g.drawString("You only caught" +score+ "inches of water", 30, 100);
+	g.drawString("You only caught " +score+ " inches of water", 85, 300);
 	g.setFont(restart);
 	g.setColor(Color.WHITE);
-	g.drawString("Press ENTER to try again", 30, 300);
+	g.drawString("Press SHIFT to try again", 110, 500);
 	
 }
 void drawMenuState(Graphics g) {
 g.drawImage(rain, 0,0, RainCatcher.WIDTH, RainCatcher.HEIGHT, null);
 g.setFont(menuFont);
 g.setColor(Color.WHITE);
-g.drawString("Water Catcher", 30, 100);
+g.drawString("Rain Catcher", 85, 150);
 g.setFont(startFont);
 g.setColor(Color.WHITE);
-g.drawString("Press ENTER to Begin", 50, 150);
+g.drawString("Press ENTER to Begin", 110, 300);
 g.setFont(instructionsFont);
 g.setColor(Color.WHITE);
-g.drawString("Press SPACE for Instructions",50 ,200);
+g.drawString("Press SPACE for Instructions",83 ,450);
 }
 void drawInstructionsState(Graphics g) {
 g.drawImage(rainpics, 0,0, RainCatcher.WIDTH, RainCatcher.HEIGHT, null);
-g.setFont(instructionsFont);
+g.setFont(instructionsTitle);
 g.setColor(Color.WHITE);
-g.drawString("Rain Catcher Instructions", 30, 100);
+g.drawString("Rain Catcher Instructions", 25, 150);
 g.setFont(instructions);
-g.drawString("Try to catch as much rain as you can by moving the bucket", 20, 150);
+g.drawString("Try to catch as much rain", 110, 275);
+g.drawString("as you can by moving the bucket", 80, 350);
 g.setFont(instructions1);
-g.drawString("Try to beat your previous score! Good luck and have fun!", 20, 200);
+g.drawString("Try to beat your previous score!", 90, 525);
+g.drawString("Good luck and have fun!", 130, 625);
 }
 @Override
 public void paintComponent(Graphics g){
@@ -177,7 +188,27 @@ public void keyPressed(KeyEvent e) {
         else if(currentState==END_STATE) {
         System.out.println("hey");
         }
-		}
+		
+
+if(e.getKeyCode()==KeyEvent.VK_SPACE) {
+	
+	if(currentState==MENU_STATE) {
+		currentState = INSTRUCTIONS_STATE;
+	}
+	else if(currentState==INSTRUCTIONS_STATE){
+		currentState = MENU_STATE;
+	}
+}
+
+if(e.getKeyCode()==KeyEvent.VK_SHIFT) {
+	
+	if(currentState==END_STATE) {
+		currentState = MENU_STATE;
+	}
+	
+}
+}
+
         
 
 @Override
